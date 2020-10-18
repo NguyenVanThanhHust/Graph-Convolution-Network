fa721c46a1ab0c8d9a4d1f809837c04c23c9577d
NguyenVanThanhHust <nguyen.van.thanh.hust@gmail.com>
2020-10-18 17:36:04 +0700

    add util function

---
+++ GCNII/utils.py
@@ -0,0 +1,54 @@
+import numpy as np
+import scipy.sparse as sp
+import torch
+import torch.nn.functional as F
+import sys
+import pickle as pkl
+import networkx as nx
+import json
+from networkx.readwrite import json_graph
+import pdb
+sys.setrecursionlimit(99999)
+
+def parse_index_file(filename):
+    """
+    Parse index file
+    """
+    index = []
+    for line in open(filename):
+        index.append(int(line.strip()))
+    return index
+
+def normalize(matrix):
+    rowsum = np.array(matrix.sum(1))
+    rowsum = (rowsum==0) *1 + rowsum
+    r_inv = np.power(rowsum, -1).flatten()
+    r_inv[np.isinf(r_inv)] = 0.
+    r_mat_inv = sp.diags(r_inv)
+    matrix = r_mat_inv.dot(matrix)
+    return matrix
+
+def sample_mask(idx, l):
+    """Create mask."""
+    mask = np.zeros(l)
+    mask[idx] = 1
+    return np.array(mask, dtype=np.bool)
+
+def sys_normalized_adjacency(adj):
+   adj = sp.coo_matrix(adj)
+   adj = adj + sp.eye(adj.shape[0])
+   row_sum = np.array(adj.sum(1))
+   row_sum=(row_sum==0)*1+row_sum
+   d_inv_sqrt = np.power(row_sum, -0.5).flatten()
+   d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.
+   d_mat_inv_sqrt = sp.diags(d_inv_sqrt)
+   return d_mat_inv_sqrt.dot(adj).dot(d_mat_inv_sqrt).tocoo()
+
+def sparse_mx_to_torch_sparse_tensor(sparse_mx):
+    """Convert a scipy sparse matrix to a torch sparse tensor."""
+    sparse_mx = sparse_mx.tocoo().astype(np.float32)
+    indices = torch.from_numpy(
+        np.vstack((sparse_mx.row, sparse_mx.col)).astype(np.int64))
+    values = torch.from_numpy(sparse_mx.data)
+    shape = torch.Size(sparse_mx.shape)
+    return torch.sparse.FloatTensor(indices, values, shape)
